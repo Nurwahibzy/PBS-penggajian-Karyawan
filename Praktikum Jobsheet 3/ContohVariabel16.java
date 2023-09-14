e695837325a6ec37bed758f51251731d15a6fe23
Lelytameyda29rpl <72966988+Lelytameyda29rpl@users.noreply.github.com>
2023-09-14 16:36:35 +0700

    Add files via upload

---
+++ Praktikum Jobsheet 3/ContohVariabel16.java
@@ -0,0 +1,16 @@
+public class ContohVariabel16 {
+    public static void main(String[] args) {
+
+        String hobi = "Membaca Novel";
+        boolean karakter = true;
+        char jenis_kelamin = 'P';
+        byte umur = 19;
+        double ipk = 4.00, tinggi = 1.56;
+        System.out.println("Hobi Saya : " + hobi);
+        System.out.println("Apakah Anda Pandai? " + karakter);
+        System.out.println("Jenis Kelamin : " + jenis_kelamin);
+        System.out.println("Umur Saya Saat Ini: " + umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi));
+
+    }
+}
