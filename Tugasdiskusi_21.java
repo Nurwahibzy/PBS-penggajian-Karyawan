30dd8ee1ad361823938cd554b257e4a325178757
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-05 19:32:30 +0700

    Membuat method baru

--- Tugasdiskusi_21.java
+++ Tugasdiskusi_21.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
+
 public class Tugasdiskusi_21 {
 
     public static void main(String[] args) {
+        login(); // Memanggil method login
 
-        String[] username = {"admin1", "pegawai2", "manajer3"};
-        String[] password = {"adminpassword1", "pegawaipassword2", "manajerpassword3"};
+    }
 
-        String nama_pegawai, jabatan, bulan_gaji;
-        int jumlah_jam_kerja, jumlah_jam_lembur, total_jam_kerja;
-        int upah = 15700, total_jam_lembur, total_gaji, gaji_pokok=2000000;
+    // Membuat method untuk login
+    static void login() {
+        String[] username = { "admin1", "pegawai2", "manajer3" };
+        String[] password = { "adminpassword1", "pegawaipassword2", "manajerpassword3" };
 
-        Scanner input16 = new Scanner(System.in);
+        Scanner input21 = new Scanner(System.in);
 
         System.out.print("Masukkan username: ");
-        String usernameInput = input16.nextLine();
-
+        String usernameInput = input21.nextLine();
         System.out.print("Masukkan password: ");
-        String passwordInput = input16.nextLine();
+        String passwordInput = input21.nextLine();
 
         boolean isAuthenticated = false;
 
@@ -30,42 +31,54 @@ public class Tugasdiskusi_21 {
 
         if (isAuthenticated) {
             System.out.println("Selamat datang, " + usernameInput + "!");
+            // Memanggil method aksesGaji()
+            aksesGaji();
+
         } else {
             System.out.println("Username dan password salah. Silakan coba lagi.");
         }
+        input21.close();
+    }
+
+    //Method yang hanya bisa diakses setelah berhasil login
+    static void aksesGaji() {
+        String nama_pegawai, jabatan, bulan_gaji;
+        int jumlah_jam_kerja, jumlah_jam_lembur, total_jam_kerja;
+        int upah = 15700, total_jam_lembur, total_gaji, gaji_pokok = 2000000;
+
+        Scanner input21 = new Scanner(System.in);
 
         System.out.println("=== MOHON MENGISI DATA DI BAWAH INI ====");
 
         System.out.print("Masukkan Nama Lengkap Anda : ");
-        nama_pegawai = input16.nextLine();
+        nama_pegawai = input21.nextLine();
         System.out.print("Masukkan Jabatan Anda : ");
-        jabatan = input16.nextLine();
+        jabatan = input21.nextLine();
         System.out.print("Masukkan Gaji Bulan Yang Akan Diambil : ");
-        bulan_gaji = input16.nextLine();
+        bulan_gaji = input21.nextLine();
         System.out.print("Masukkan Jumlah Jam Kerja Anda : ");
-        jumlah_jam_kerja = input16.nextInt();
+        jumlah_jam_kerja = input21.nextInt();
         System.out.print("Masukkan Jumlah Jam Lembur Anda Jika Ada : ");
-        jumlah_jam_lembur = input16.nextInt();
-        
+        jumlah_jam_lembur = input21.nextInt();
+
         switch (jabatan) {
             case "Koki":
-            gaji_pokok = 2800000;
-            break;
+                gaji_pokok = 2800000;
+                break;
             case "Kasir":
-            gaji_pokok = 2500000;
-            break;
+                gaji_pokok = 2500000;
+                break;
             case "Pramusaji":
-            gaji_pokok = 2300000;
-            break;
+                gaji_pokok = 2300000;
+                break;
 
         }
 
-        total_jam_kerja = jumlah_jam_kerja * 26;//rata - rata jumlah dengan 6 hari kerja seminggu
+        total_jam_kerja = jumlah_jam_kerja * 26;// rata - rata jumlah dengan 6 hari kerja seminggu
         total_jam_lembur = jumlah_jam_lembur * upah;
         total_gaji = gaji_pokok + total_jam_kerja + total_jam_lembur;
         System.out.println("Total Gaji Yang Anda Terima Adalah : " + "Rp." + total_gaji);
 
-        input16.close();
+        input21.close();
     }
-    
 }
